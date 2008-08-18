206309d900f6039a9ba9ed3a3687ee6a4fceed6f
Victor Berchet <victor@suumit.com>
2008-08-18 06:46:03 +0000

    IGN: bug fix for div border height

--- visugps/php/vg_directproxy.php
+++ visugps/php/vg_directproxy.php
@@ -24,37 +24,19 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 Copyright (c) 2007-2008 Victor Berchet, <http://www.victorb.fr>
 
 */
-    // Code base on Geoportal reference
-    // https://api.ign.fr/geoportail/doc/fr/webmaster/proxy_wfs.html
-
-    // Get the REST call path from the AJAX application
-    // Is it a POST or a GET?
-    $url = isset($_POST['url']) ? $_POST['url'] : $_GET['url'];
+    if (isset($_GET['url'])) {
+        $url = $_GET['url'];
+    } else {
+        exit;
+    }
 
     // Open the Curl session
     $session = curl_init($url);
-
-    // If it's a POST, put the POST data in the body
-    if (isset($_POST['url'])) {
-        $postvars = '';
-        while ($element = current($_POST)) {
-            $postvars .= key($_POST).'='.$element.'&';
-            next($_POST);
-        }
-        curl_setopt ($session, CURLOPT_POST, true);
-        curl_setopt ($session, CURLOPT_POSTFIELDS, $postvars);
-    }
-
-    // Don't return HTTP headers. Do return the contents of the call
     curl_setopt($session, CURLOPT_HEADER, false);
     curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
-
-    // Make the call
     $xml = curl_exec($session);
 
-    // The web service returns XML. Set the Content-Type appropriately
     header("Content-Type: text/xml");
-
     echo $xml;
     curl_close($session);
 

4518c17b427de5da66897fd14ce5099a6078990c
Victor Berchet <victor@suumit.com>
2010-06-10 12:13:32 +0000

    adding DB indexes

--- visugps/sql/mvgps.sql
+++ visugps/sql/mvgps.sql
@@ -11,8 +11,11 @@ CREATE TABLE flight (
   `end` datetime default NULL,
   utc tinyint(1) NOT NULL default '1',
   ua char(40) default NULL,
-  PRIMARY KEY  (id)
-) ENGINE=MyISAM AUTO_INCREMENT=105 DEFAULT CHARSET=latin1;
+  PRIMARY KEY  (id),
+  KEY pilotID (pilotID),
+  KEY `start` (`start`),
+  KEY `end` (`end`)
+) ENGINE=MyISAM AUTO_INCREMENT=1006 DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -33,6 +36,19 @@ CREATE TABLE flightInfo (
 -- --------------------------------------------------------
 
 -- 
+-- Table structure for table `location`
+-- 
+
+CREATE TABLE location (
+  lieu varchar(30) NOT NULL,
+  latitude float NOT NULL,
+  longitude float NOT NULL,
+  KEY latitude (latitude,longitude)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+-- 
 -- Table structure for table `pilot`
 -- 
 
@@ -42,8 +58,9 @@ CREATE TABLE pilot (
   `name` char(30) NOT NULL,
   email char(60) NOT NULL,
   PRIMARY KEY  (id),
-  UNIQUE KEY pseudo (pseudo,`name`)
-) ENGINE=MyISAM AUTO_INCREMENT=46 DEFAULT CHARSET=latin1;
+  UNIQUE KEY pseudo (pseudo,`name`),
+  KEY `name` (`name`)
+) ENGINE=MyISAM AUTO_INCREMENT=101 DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -52,15 +69,17 @@ CREATE TABLE pilot (
 -- 
 
 CREATE TABLE `point` (
-  id int(10) unsigned NOT NULL auto_increment,
+  id int(11) NOT NULL auto_increment,
   flightId int(10) unsigned NOT NULL,
   latitude float NOT NULL,
   longitude float NOT NULL,
   elevation int(11) NOT NULL,
   `time` datetime NOT NULL,
   `timestamp` timestamp NOT NULL default CURRENT_TIMESTAMP,
-  KEY flightId (flightId)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  PRIMARY KEY  (id),
+  KEY flightId (flightId),
+  KEY `time` (`time`)
+) ENGINE=MyISAM AUTO_INCREMENT=350704 DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -73,3 +92,4 @@ CREATE TABLE test (
   `time` datetime NOT NULL,
   PRIMARY KEY  (id)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_german2_ci;
+

ead5273922ec3e73cfe823c29537a570d64f5ba8
Victor Berchet <victor.berchet@yahoo.com>
2011-05-18 19:02:33 +0200

    Add a js proxy to display track on XCPlanner

---
+++ visugps/php/vg_proxyjs.php
@@ -0,0 +1,46 @@
+<?php
+/*
+Script: vg_proxy.php
+        Download a GPS track and return it in a JSON format
+
+License: GNU General Public License
+
+This file is part of VisuGps
+
+VisuGps is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+VisuGps is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with VisuGps; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+Copyright (c) 2007-2008 Victor Berchet, <http://www.victorb.fr>
+
+*/
+
+
+header('Content-type: text/plain; charset=ISO-8859-1');
+header('Cache-Control: no-cache, must-revalidate');
+header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
+
+require('vg_cfg.inc.php');
+require('vg_tracks.php');
+
+$track = array();
+
+if (isset($_GET['track'])) {
+    $track = MakeTrack($_GET['track']);
+} else if (isset($_GET['trackid'])) {
+    $track = GetDatabaseTrack(intval($_GET['trackid']));
+}
+
+echo "var trackData = JSON.parse(" . @json_encode($track) . ');';
+
+?>
